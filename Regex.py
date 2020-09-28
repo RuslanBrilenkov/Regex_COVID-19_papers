3ad38ca336ffec4c6529a24619a5afd49709edb6
RuslanBrilenkov <ruslan.brilenkov@gmail.com>
2020-09-29 00:09:23 +0200

    modification to the code - removing names with apostrophe

--- Regex.py
+++ Regex.py
@@ -6,13 +6,18 @@ Their grandfather, Andrew, is 95 years old, and Andrew's father, Arthur, is 120.
 # printing the text
 print(text)
 
+# Searching for the ages of people
+ages = re.findall(r'\d{1,3}',text)
+# printing the output
+print("\n{}".format(ages))
+
 # Searching for the names of people 
-names = re.findall(r'[A-Z][a-z]+', text1)
+names = re.findall(r'[A-Z][a-z]+', text)
 #names = re.findall(r"\b[A-Z][a-z]*", text1)
 # printing the output
 print("\n{}".format(names))
 
-# Searching for the ages of people
-ages = re.findall(r'\d{1,3}',text1)
+# Using a negative lookahead to exclude ownership form, such as Andrew's
+names2 = re.findall(r'\b(?![A-Z][a-z]+\'s\b)[A-Z][a-z]+\b', text)
 # printing the output
-print("\n{}".format(ages))
+print("\n{}".format(names2))
