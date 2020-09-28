232d6cd9192b9a79e76c4e0207e62f3d109338ef
RuslanBrilenkov <ruslan.brilenkov@gmail.com>
2020-09-28 23:10:57 +0200

    second commit

--- Regex.py
+++ Regex.py
@@ -1,18 +1,18 @@
 import re
 
-
-text1 = "Isabella is 16 years old, and Jonas is 24 years old. \
+# An example text to process
+text = "Isabella is 16 years old, and Jonas is 24 years old. \
 Their grandfather, Andrew, is 95 years old, and Andrew's father, Arthur, is 120."
+# printing the text
+print(text)
 
-print(text1)
-
+# Searching for the names of people 
 names = re.findall(r'[A-Z][a-z]+', text1)
 #names = re.findall(r"\b[A-Z][a-z]*", text1)
+# printing the output
+print("\n{}".format(names))
 
-print()
-print(names)
-
+# Searching for the ages of people
 ages = re.findall(r'\d{1,3}',text1)
-
-print()
-print(ages)
+# printing the output
+print("\n{}".format(ages))
